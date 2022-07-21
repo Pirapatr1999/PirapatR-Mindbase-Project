aff6eb2aa80263ab5b31e52fbbab1166bde3c7bf
PirapatR <108516431+Pirapatr1999@users.noreply.github.com>
2022-07-21 15:46:01 +0700

    [master] new PageOne&pageTwo && html-show-Page-One

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PageOneComponent } from './page-one/page-one.component';
+import { PageTwoComponent } from './page-two/page-two.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageOneComponent,
+    PageTwoComponent
   ],
   imports: [
     BrowserModule
